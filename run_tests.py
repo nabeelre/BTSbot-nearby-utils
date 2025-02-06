69b6786bcc63cecceb38eed9f1c7881724711d4f
Nabeel Rehemtulla <nabeelre@gmail.com>
2025-02-06 13:55:51 -0600

    adding new test cases

--- run_tests.py
+++ run_tests.py
@@ -21,18 +21,38 @@ lab_40Mpc_filterid = 1191
 lab_60Mpc_filterid = 1194
 lab_80Mpc_filterid = 1195
 
+latest_SEDM = (lab_60Mpc_filterid, "ig5xn5")
+
+# TODO write function for running and comparing output of two filters
+
 
 def __main__():
     # Create directory for output
     run_t_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     os.makedirs(f"logs/{run_t_stamp}/")
-    curr_filterid = 1182
+    curr_filterid = latest_SEDM[0]
+    curr_filerver = latest_SEDM[1]
 
     # Create TestCases
-    TC_sn2024jlf = TestCase(
-        2460456.0, 2460460.0, neg_ids=[], pos_ids=["ZTF24aaozxhx"],
-        notes="Recover 2024jlf / ZTF24aaozxhx", name="SN2024jlf"
-    )
+    TC_z001 = [
+        TestCase(
+            2460456.0, 2460460.0, neg_ids=[], pos_ids=["ZTF24aaozxhx"],
+            notes="Recover 2024jlf", name="SN 2024jlf"
+        ),
+        TestCase(
+            60690.0, 60695.0, neg_ids=[], pos_ids=["ZTF25aacerkv"],
+            notes="Recover ZTF25aacerkv", name="ZTF25aacerkv"
+        ),
+        TestCase(
+            60688.0, 60693.0, neg_ids=[], pos_ids=["ZTF25aabylkr"],
+            notes="Recover ZTF25aabylkr, although has slightly spotty coverage",
+            name="ZTF25aabylkr"
+        ),
+        TestCase(
+            60688.0, 60695.0, neg_ids=[], pos_ids=["ZTF25aabxywm"],
+            notes="ZTF25aabxywm", name="ZTF25aabxywm"
+        )
+    ]
 
     TC_bad_hist = [
         TestCase(
@@ -52,6 +72,7 @@ def __main__():
         )
     ]
 
+    # No non-detections prior to first alert
     TC_no_lims = [
         TestCase(
             60631.0, 60635.0, neg_ids=["ZTF24abszdqt"], pos_ids=[],
@@ -67,17 +88,49 @@ def __main__():
         TestCase(
             60620.0, 60624.0, neg_ids=["ZTF24abrhrue"], pos_ids=[],
             notes="ZTF24abrhrue", name="bogus1"
+        ),
+        TestCase(
+            60675.0, 60690.0, neg_ids=["ZTF22aacffrr"], pos_ids=[],
+            notes="ZTF22aacffrr, one alert with high drb, medium bts", name="bogus2"
+        ),
+        TestCase(
+            60676.0, 60682.0, neg_ids=["ZTF25aaastwn"], pos_ids=[],
+            notes="ZTF25aaastwn, medium drb", name="bogus2"
         )
     ]
 
+    # Only non-detections (2.5-10 days since first alert), trigger SEDM but not others
+    TC_loose_lims = [
+        TestCase(
+            60704.0, 60708.0, neg_ids=[], pos_ids=["ZTF25aadlcbi"],
+            notes="ZTF25aadlcbi, 8 days between first alert and last non-det",
+            name="loose_lims1"
+        ),
+        TestCase(
+            60704.0, 60708.0, neg_ids=[], pos_ids=["ZTF25aadhlrs"],
+            notes="ZTF25aadhlrs, 7 days between first alert and last non-det",
+            name="loose_lims2"
+        ),
+        TestCase(
+            60690.0, 60695.0, neg_ids=[], pos_ids=["ZTF25aaccmjq"],
+            notes="ZTF25aaccmjq, >3 days between first alert and last non-det",
+            name="ZTF25aaccmjq"
+        )
+    ]
+
+    # ZTF25aadlqhw one det, poor coverage
+    # ZTF25aabmhly one det, good coverage
+    # ZTF25aaaqfrf TODO - how to avoid deep drilling
+
     # Evaluate all TestCases
-    TCs = [TC_sn2024jlf] + TC_bad_hist + TC_bad_assosciation + TC_no_lims + TC_bogus
+    TCs = TC_z001 + TC_bad_hist + TC_bad_assosciation + TC_no_lims + TC_bogus + TC_loose_lims
     print(f"Evaluating {len(TCs)} TestCases")
     statuses = []
     for TC in TCs:
         statuses += [TC.evaluate_test(
             Kowalski=k, run_name=run_t_stamp,
-            filterid=curr_filterid, verbose=True
+            filterid=curr_filterid, filterver=curr_filerver,
+            verbose=True
         )]
 
     if any(statuses):
